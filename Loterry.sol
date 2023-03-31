d8d7bac7b67838cccfa3c851e23e1dd308d45d1f
hanshal101 <82961842+hanshal101@users.noreply.github.com>
2023-04-01 00:51:52 +0530

    Neatly explained the code written

--- Loterry.sol
+++ Loterry.sol
@@ -10,20 +10,29 @@ contract token{
         manager = msg.sender; //global variable
     }
 
+
+    //Get minimum ether value of 1
+
     receive() external payable{
         require(msg.value==1 ether);
         customer.push(payable(msg.sender));
     }
+
+    //Require only works for the account which is declared - here its the manager account
 
     function getBalance() public view returns(uint){
         require(msg.sender==manager);
         return address(this).balance;
     }
 
+    //Getting a random hash using prebuilt library 'keccak256' 
+
     function random() public view returns(uint){
         return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, customer.length)));
     }
 
+
+     //Lets get the Winner now and transfer all the money to it
     function getWinner() public 
     {
         require(msg.sender==manager);
